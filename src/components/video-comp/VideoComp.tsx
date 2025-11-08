448ef6c10aafcc0a601bc58f831f112ececdde71
rukadeveloper <kansaki135@gmail.com>
2025-11-08 03:12:49 -0800

    add skeleton ui

--- src/components/video-comp/VideoComp.tsx
+++ src/components/video-comp/VideoComp.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const VC = styled.div`
   width: 100%;
   padding: 30px 60px;
+  position: relative;
   img {
     margin-bottom: 30px;
   }
@@ -15,9 +17,37 @@ const VC = styled.div`
 `;
 
 const VideoComp = () => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <VC>
-      <video autoPlay muted loop playsInline>
+      {loading && (
+        <div
+          style={{
+            width: "calc(100% - 60px)",
+            aspectRatio: 1.76231 / 1,
+            backgroundColor: "#E8EEF2",
+            position: "absolute",
+            left: "30px",
+            right: "30px",
+            top: "30px",
+            bottom: "30px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          로딩중입니다..
+        </div>
+      )}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        onLoadStart={() => setLoading(true)}
+        onCanPlay={() => setLoading(false)}
+      >
         <source src={"/video.mp4"} type="video/mp4" />
       </video>
     </VC>
