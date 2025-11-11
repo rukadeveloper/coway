752620d22f31473a287fac9f614d161a6d1f6438
rukadeveloper <kansaki135@gmail.com>
2025-11-11 04:57:38 -0800

    문구추가

--- src/components/logo/MainLogo.tsx
+++ src/components/logo/MainLogo.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 const ML = styled.div`
+  h2 {
+    text-align: center;
+    color: #44a7e0;
+  }
   @media (max-width: 500px) {
     img {
       width: 160px;
@@ -12,6 +16,7 @@ const MainLogo = () => {
   return (
     <ML style={{ marginBottom: "20px" }}>
       <img src={"/coway_logo.png"} alt="logo" width={240} />
+      <h2>코웨이 정수기</h2>
     </ML>
   );
 };
