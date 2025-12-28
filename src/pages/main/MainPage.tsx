b1f7adcf4cd7e1fd93ae6fc208ca48b2539a2f2f
rukadeveloper <kansaki135@gmail.com>
2025-12-27 22:09:22 -0800

    component delete

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -2,14 +2,10 @@ import styled from "styled-components";
 
 import FreeButton from "../../components/free-btn/FreeButton";
 import GridComp from "../../components/grid-comp/GridComp";
-import ImageBanner from "../../components/image-banner/ImageBanner";
-import ImageMainSlide from "../../components/image-main-slide/ImageMainSlide";
-import ImageSlide from "../../components/image-slide/ImageSlide";
 import MainLogo from "../../components/logo/MainLogo";
 import Matrix from "../../components/matrix/Matrix";
 import TopInput from "../../components/top-input/TopInput";
 import UseContact from "../../components/use/UseContact";
-import VideoComp from "../../components/video-comp/VideoComp";
 import ZehuCard from "../../components/zehu-card/ZehuCard";
 import CallButton from "../../components/call-btn/CallButton";
 
